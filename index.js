3304ac6eac273f4df15deec78d9eed128ff21d03
Gabriel <gabrielwambugu13@yahoo.co.uk>
2022-12-23 11:39:20 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Gabriel Wambugu';
+
+const welcomeMessage = '"Welcome to Flatbook, "' + " " + currentUser + "!" ;
+
+const excitedWelcomeMessage = '"WELCOME TO FLATBOOK, "' + " " + currentUser.toUpperCase() + "!";
+
+const shortGreeting = '"Welcome, ":' + currentUser.slice(0,1) + "!";
+
+const greeting = "Love to dress up";
+let message = greeting.slice(5);
